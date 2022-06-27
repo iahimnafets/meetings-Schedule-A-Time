7b156d292f91d38cf94a4f314a3b195115df91b4
iahimnafets <80846557+iahimnafets@users.noreply.github.com>
2022-06-27 16:43:33 +0200

    Update MeetingCalendarResource.java

--- meetings/src/main/java/com/aqka/interviews/controller/MeetingCalendarResource.java
+++ meetings/src/main/java/com/aqka/interviews/controller/MeetingCalendarResource.java
@@ -1,9 +1,7 @@
+
 package com.aqka.interviews.controller;
 
-import com.aqka.interviews.entities.Meeting;
-import com.aqka.interviews.entities.MeetingDTO;
-import com.aqka.interviews.entities.MeetingRequest;
-import com.aqka.interviews.entities.MeetingResponse;
+import com.aqka.interviews.entities.*;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,21 +16,21 @@ public interface MeetingCalendarResource {
 
     @Operation( summary =
             "    Add a working time for example:  0900 1730 or 0800 1630  means ( from 9:00 to 17:30 or from 8:00 to 16:30 )" )
-    public ResponseEntity<String> addWorkTime(@RequestParam( name = "timeStart" , required = true ) final String timeStart,
+    public ResponseEntity<Response> addWorkTime(@RequestParam( name = "timeStart" , required = true ) final String timeStart,
                                               @RequestParam( name = "timeEnd", required = true   ) final String timeEnd );
 
 
     @Operation( summary = " Book a new time in this format, example: \n" +
                           "dateRequest EmployeeId      : 2011-03-17 10:17:06 EMP001\n" +
                            "meetingStartTime Duration  : 2011-03-21 09:00 2 "  )
-    public ResponseEntity<String> addNewMeeting( @RequestBody final MeetingRequest meetingRequest  );
+    public ResponseEntity<Response> addNewMeeting( @RequestBody final MeetingRequest meetingRequest  );
 
     @Operation( summary = " Find bookings by single day, example: \n" +
             "dateMeeting: 21-03-2011\n"  )
-    public ResponseEntity<Collection<Meeting>> getMeetingsByDate(
+    public ResponseEntity<Response> getMeetingsByDate(
             @RequestParam( name = "dateMeeting",  required = false ) final String dateMeeting);
 
     @Operation( summary = " Find all bookings  "  )
-    public ResponseEntity<Collection<MeetingResponse>> getAllMeetings() ;
+    public ResponseEntity<Response> getAllMeetings() ;
 
 }
