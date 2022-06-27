b8c48c80c6cd5e3a78f4a28d3241d2e0b1c5d851
iahimnafets <80846557+iahimnafets@users.noreply.github.com>
2022-06-27 16:43:04 +0200

    Update MeetingCalendarController.java

--- meetings/src/main/java/com/aqka/interviews/controller/MeetingCalendarController.java
+++ meetings/src/main/java/com/aqka/interviews/controller/MeetingCalendarController.java
@@ -1,17 +1,17 @@
 package com.aqka.interviews.controller;
 
 
-import com.aqka.interviews.entities.Meeting;
-import com.aqka.interviews.entities.MeetingDTO;
-import com.aqka.interviews.entities.MeetingRequest;
-import com.aqka.interviews.entities.MeetingResponse;
+import com.aqka.interviews.entities.*;
 import com.aqka.interviews.service.MeetingService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Map;
 
 
 @RestController
@@ -33,37 +33,67 @@ public class MeetingCalendarController implements MeetingCalendarResource
 
     @Override
     @PutMapping ( value = "/add-work-time" )
-    public ResponseEntity<String> addWorkTime(@RequestParam( name = "timeStart" , required = true ) final String timeStart,
+    public ResponseEntity<Response> addWorkTime(@RequestParam( name = "timeStart" , required = true ) final String timeStart,
                                               @RequestParam( name = "timeEnd", required = true   ) final String timeEnd )
     {
         matchService.addWorkTime( timeStart, timeEnd );
-        return ResponseEntity.ok("Work Time added successfully" );
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .status(HttpStatus.OK)
+                        .data(Map.of("add-work-time", "Work Time added successfully" ))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
     }
 
 
     @Override
     @PutMapping( value = "/add-new-meeting" )
-    public ResponseEntity<String> addNewMeeting( @RequestBody final MeetingRequest meetingRequest  )
+    public ResponseEntity<Response> addNewMeeting( @RequestBody final MeetingRequest meetingRequest  )
     {
         matchService.addNewMeeting( meetingRequest );
-        return ResponseEntity.ok("Meeting added correctly!");
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .status(HttpStatus.OK)
+                        .data(Map.of("add-new-meeting", "Meeting added correctly!" ))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
     }
 
 
 
     @Override
     @GetMapping( value = "/get-meetings-by-date" )
-    public ResponseEntity<Collection<Meeting>> getMeetingsByDate(
+    public ResponseEntity<Response> getMeetingsByDate(
                        @RequestParam( name = "dateMeeting",  required = false ) final String dateMeeting) {
-        Collection<Meeting> result = matchService.getMeetingsByDate ( dateMeeting );
-        return ResponseEntity.ok(result );
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .status(HttpStatus.OK)
+                        .data(Map.of("get-meetings-by-date", matchService.getMeetingsByDate ( dateMeeting ) ))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
     }
 
     @Override
     @GetMapping( value = "/get-all-meetings" )
-    public ResponseEntity<Collection<MeetingResponse>> getAllMeetings() {
-        Collection<MeetingResponse> result = matchService.getAllMeetings ( );
-        return ResponseEntity.ok(result );
+    public ResponseEntity<Response> getAllMeetings() {
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .status(HttpStatus.OK)
+                        .data(Map.of("get-all-meetings", matchService.getAllMeetings ( ) ))
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
     }
 
 }
