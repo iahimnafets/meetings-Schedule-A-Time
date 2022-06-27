a4ca2198ea34eb1d8294c295a9df2fd767a8ca52
iahimnafets <80846557+iahimnafets@users.noreply.github.com>
2022-06-27 16:12:16 +0200

    Update MeetingUtil.java

--- meetings/src/main/java/com/aqka/interviews/service/MeetingUtil.java
+++ meetings/src/main/java/com/aqka/interviews/service/MeetingUtil.java
@@ -110,5 +110,14 @@ public class MeetingUtil {
         }
         return result;
     }
+    
+    public static boolean isStringInt(String s){
+        try{
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException ex){
+            return false;
+        }
+    }
 
 }
